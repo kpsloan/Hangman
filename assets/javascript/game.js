7627d5f686ba5d989639f9296c674f56e2bb4027
Jest <kyle.p.sloan@gmail.com>
2018-09-30 23:48:29 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,135 +1,140 @@
-window.onload = function () {
 
-    var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var game = {
 
-    var lives;
-    var counter;
-    var space;
-    var score = 0;
-    var categories;
-    var selectedCategory;
-    var word;
-    var guess;
-    var guessed = [];
-    var showLives = document.getElementById("lives");
-    var showCatagory = document.getElementById("selectedCategory");
+    // this can come from an array and be randomly selected at startup
+    category: '',
+    word: '',
+    categories: {
+        'Killers': [
+            "michael myers",
+            "jason voorhees",
+            "freddy krueger",
+            "leatherface",
+            "norman bates",
+            "pinhead",
+            "count dracula",
+            "pennywise"
+        ],
+        'Film Titles': [
+            "alien",
+            "halloween",
+            "psycho",
+            "nightmare on elm street",
+            "texas chainsaw massacre",
+            "hellraiser",
+            "the thing",
+            "jaws"
+        ],
+        'Monsters': [
+            "xenomorph",
+            "vampire",
+            "zombie",
+            "werewolf",
+            "godzilla",
+            "frankenstein",
+            "gremlins",
+            "cenobites"
+        ]
+    },
+    guesses: [],
+    lives: 6,
+    hasWon: false,
 
+    getCategoryWord: function () {
+        var keys = Object.keys(this.categories)
+        var category = keys[Math.floor(keys.length * Math.random())];
 
+        var words = this.categories[category];
 
-    // Select catagory
-    var selectCat = function () {
-        if (selectedCategory === categories[0]) {
-            topic.innerHTML = "Killers";
-        } else if (selectedCategory === categories[1]) {
-            topic.innerHTML = "Film Titles";
-        } else if (selectedCategory === categories[2]) {
-            topic.innerHTML = "Monsters";
-        }
-    }
+        this.category = category;
+        this.word = words[Math.floor(words.length * Math.random())];
 
-    // Create guesses list
-    result = function () {
-        wordHolder = document.getElementById('box');
-        correct = document.createElement('ul');
+    },
 
-        for (var i = 0; i < word.length; i++) {
-            correct.setAttribute('id', 'my-word');
-            guess = document.createElement('li');
-            guess.setAttribute('class', 'guess');
-            if (word[i] === " ") {
-                guess.innerHTML = " ";
-                space = 1;
+    drawBlanks: function () {
+        var e = document.querySelector('#blanks');
+        var output = [];
+        var isSolved = true;
+        for (var i = 0; i < this.word.length; i++) {
+            if (this.guesses.indexOf(this.word.charAt(i)) > -1) {
+                //found a match
+                output.push(this.word.charAt(i).toUpperCase());
+            } else if (this.word.charAt(i) == ' ') {
+                output.push(' ');
             } else {
-                guess.innerHTML = "_";
-                space = 1;
-            }
-
-            guessed.push(guess);
-            wordHolder.appendChild(correct);
-            correct.appendChild(guess);
-        }
-    }
-
-
-    // Show lives
-    comments = function () {
-        showLives.innerHTML = "You have " + lives + " chances";
-        if (lives < 1) {
-            showLives.innerHTML = "You Died";
-        }
-        for (var i = 0; i < guessed.length; i++) {
-            if (counter + space === guessed.length) {
-                wins++;
-                showLives.innerHTML = "Lucky";
+                output.push('_');
+                isSolved = false;
             }
         }
-    }
 
-    wins = function () {
-        document.querySelector("#wins").innerHTML = "Escaped Death " + score;
-    }
+        e.innerHTML = output.join(' ');
 
-    // OnKey Function
-
-    window.onkeypress = function () {
-        if(event.keyCode>64 && event.keyCode<91){
-            var found=false;
-            var previouslyEntered=false;
-            var input=String.fromCharCode(event.keyCode).toLowerCase();
-                    
-            for(i=0;i<previousGuesses.length;i++){
-                if(input==previousGuesses[i]){
-                    previouslyEntered=true;
-                }
-            }
+        if (isSolved) {
+            this.hasWon = true;
         }
-    checkGuess = function (guess) {
+        var lives = document.getElementById("lives");
+        lives.innerHTML = "You have " + this.lives + " chances";
+
+        var topic = document.getElementById('topic');
+        topic.innerHTML = this.category;
+    },
+
+    checkGuess: function (guess) {
         if (this.word.indexOf(guess) > -1) {
             // got one right
-            this.guess.push(guess);
+            this.guesses.push(guess);
             return true;
         } else {
             //increment wrong counter
-            this.guessed++;
+            this.lives--;
             return false;
         }
+    },
+
+    play: function (guess) {
+        // check empty guess box, this isn't dark souls
+
+        //check for valid letter i guess
+        var lives = document.getElementById("lives");
+        this.checkGuess(guess)
+        lives.innerHTML = "You have " + this.lives + " chances";
+
+        this.drawBlanks();
+
+        if (this.lives < 1) {
+            lives.innerHTML = "You Died";
+            var res = confirm('Play again?');
+            if (res) {
+                this.newGame();
+            }
+        }
+
+        if (this.hasWon) {
+            lives.innerHTML = "Lucky";
+            var res = confirm('Play again?');
+            if (res) {
+                this.newGame();
+            }
+        }
+    },
+
+    newGame: function () {
+        this.lives = 6;
+        this.hasWon = false;
+        this.guesses = [];
+        this.getCategoryWord();
+        this.drawBlanks();
     }
-
-
-
-    //Play  
-    play = function () {
-        categories = [
-            ["michael myers", "jason voorhees", "freddy krueger", "leatherface", "norman bates", "pinhead", "count dracula", "pennywise"],
-            ["alien", "halloween", "psycho", "nightmare on elm street", "texas chainsaw massacre", "hellraiser", "the thing", "jaws"],
-            ["xenomorph", "vampire", "zombie", "werewolf", "godzilla", "frankenstein", "gremlins", "cenobites"]
-        ];
-
-        selectedCategory = categories[Math.floor(Math.random() * categories.length)];
-        word = selectedCategory[Math.floor(Math.random() * selectedCategory.length)];
-        word = word.replace(/\s/g, "_");
-        console.log(word);
-
-
-        guessed = [];
-        lives = 6;
-        counter = 0;
-        space = 0;
-        result();
-        comments();
-        selectCat();
-        wins();
-    }
-
-    play();
-
-    }
-
 }
-    
 
+window.onkeypress = function () {
+    input = String.fromCharCode(event.keyCode).toLowerCase();
+    game.play(input);
+}
 
-
+//startup
+game.getCategoryWord();
+game.drawBlanks();
 
 
 
